837f4bea5a669d2f9beeb00d457c19cbfb01b7b6
Hank <auroramsdn6@hotmail.com>
2018-05-16 16:53:41 +0800

    建構式注入

---
+++ 0518Demo/Service/LogAnalyer/LogAnalyer.cs
@@ -0,0 +1,23 @@
+﻿using _0518Demo.Service.ExtensionManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _0518Demo.Service.LogAnalyer
+{
+    public class LogAnalyer
+    {
+        private IExtensionManager manager;
+
+        public LogAnalyer(IExtensionManager mgr)
+        {
+            this.manager = mgr;
+        }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            return manager.IsValid(fileName);
+        }
+    }
+}
---
+++ 0518DemoTests/Service/LogAnalyer/LogAnalyerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0518Demo.Service.LogAnalyer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _0518Demo.Service.ExtensionManager;
+
+namespace _0518Demo.Service.LogAnalyer.Tests
+{
+    [TestClass()]
+    public class LogAnalyerTests
+    {
+        [TestMethod()]
+        public void IsValidFileName_NameSupportedExtension_ReturnsTrue()
+        {
+            FakeExtensionManager myFakeManager = new FakeExtensionManager();
+
+            myFakeManager.WillBeValid = true;
+
+            LogAnalyer log = new LogAnalyer(myFakeManager);
+            bool result = log.IsValidLogFileName("short.txt");
+
+            Assert.IsTrue(result);
+        }
+    }
+
+
+    public class FakeExtensionManager : IExtensionManager
+    {
+        public bool WillBeValid = false;
+
+        public bool IsValid(string fileName)
+        {
+            return WillBeValid;
+        }
+    }
+}
